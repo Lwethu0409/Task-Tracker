e1e85c6ff644a4c639d6f21ced96a535606710ae
Thandolwethu Thwala <Thandolwethu@192.168.1.18>
2026-06-24 16:00:29 +0200

    Initiated html structure

---
+++ script.js
@@ -0,0 +1,5 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskButton = document.getElementById('addTaskButton');
+const taskList = document.getElementById('taskList');
+
+console.log(taskInput, addTaskButton, taskList);
